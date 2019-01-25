32cc26749fbd65a76bb78677058453d1bfa6ba91
Loengry Sequeira <loengry.sequeira@avantica.net>
2019-01-25 17:36:10 -0600

    embed google form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dcalc';
+  formURl: SafeResourceUrl;
+  formSource = 'https://docs.google.com/forms/d/e/1FAIpQLSeAovHRsG4I5-ubQqObSW1-hgLai9ceV4LbS1fbimIa8pCm3g/viewform?embedded=true';
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.formURl = this.sanitizer.bypassSecurityTrustResourceUrl(this.formSource);
+  }
+
+  getFormSource() {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(this.formSource);
+  }
 }
